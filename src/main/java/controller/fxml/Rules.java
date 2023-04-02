8e50475a621a04343652c87320c6c736af7842d8
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-04-02 12:23:58 +0200

    fixed Rules layout

---
+++ src/main/java/controller/fxml/Rules.java
@@ -0,0 +1,23 @@
+package controller.fxml;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Rules {
+    private final SceneHandler sceneHandler = SceneHandler.getInstance();
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    void backToMainMenu(final ActionEvent event) throws IOException {
+        if (sceneHandler.getPrimaryStage() == null) {
+            sceneHandler.setPrimaryStage((Stage) returnButton.getScene().getWindow());
+        }
+        sceneHandler.openMainMenu(event);
+    }
+}
