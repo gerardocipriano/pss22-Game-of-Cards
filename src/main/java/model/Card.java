4557f9b3d1f8a52528e8644eb4e581ee6dee07ae
FIRST_NAME LAST_NAME <massimilian.battelli@studio.unibo.it>
2023-04-06 17:40:04 +0200

    Refactor Card.java

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -1,6 +1,5 @@
 package model;
 
-import java.util.Objects;
 
 public class Card {
     private String name;
@@ -21,37 +20,6 @@ public class Card {
         this.moved = moved;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-
-        final Card other = (Card) obj;
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-
-        if (this.value != other.value) {
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 71 * hash + Objects.hashCode(this.name);
-        hash = 71 * hash + this.value;
-        return hash;
-    }
-
-    @Override
     public String toString() {
         return this.name + " - " + this.value;
     }
