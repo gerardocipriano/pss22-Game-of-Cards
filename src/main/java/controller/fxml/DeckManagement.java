6a72cfb67a015f1439c14667b51ba82e1ce4c413
FIRST_NAME LAST_NAME <massimilian.battelli@studio.unibo.it>
2023-04-06 16:10:43 +0200

    Add card select functionality

--- src/main/java/controller/fxml/DeckManagement.java
+++ src/main/java/controller/fxml/DeckManagement.java
@@ -7,13 +7,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.Card;
 
-// Il controller implementa l'interfaccia Initializable la quale richiede
-// che venga definito il metodo 'initialize' eseguito automaticamente
-// quando il file FXML viene caricato 
 public class DeckManagement implements Initializable {
     private final SceneHandler sceneHandler = SceneHandler.getInstance();
 
@@ -31,12 +30,38 @@ public class DeckManagement implements Initializable {
     @FXML
     private ListView<Card> rightList;
 
+    @FXML
+    private ListView<Card> centerList;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        rightList.setCellFactory(param -> new ListCell<Card>() {
+            @Override
+            protected void updateItem(Card card, boolean empty) {
+                super.updateItem(card, empty);
+                if (empty || card == null) {
+                    setText(null);
+                } else {
+                    VBox container = new VBox();
+                    Button cardButton = new Button("Add");
+                    cardButton.setOnAction(event -> {
+                        if (!card.isMoved()) {
+                            centerList.getItems().add(card);
+                            rightList.getItems().remove(card);
+                            card.setMoved(true);
+                        }
+                    });
+                    container.getChildren().addAll(new javafx.scene.control.Label(card.toString()), cardButton);
+                    setGraphic(container);
+                }
+            }
+        });
         rightList.getItems().addAll(
-            new Card("Carta 1", 10),
-            new Card("Carta 2", 5),
-            new Card("Carta 3", 8)
-    );
+            new Card("Card 1", 10),
+            new Card("Card 2", 5),
+            new Card("Card 3", 8)
+        );
+       
     }
+    
 }
--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -3,6 +3,7 @@ package model;
 public class Card {
     private String name;
     private int power;
+    private boolean moved = false;
 
     public Card(String name, int power) {
         this.name = name;
@@ -19,6 +20,14 @@ public class Card {
 
     @Override
     public String toString() {
-        return "Name: " + name + "\n" + "power: " + power;
+        return "Name: " + name + "\n" + "Power: " + power;
+    }
+
+    public boolean isMoved() {
+        return this.moved;
+    }
+
+    public void setMoved(boolean b) {
+        this.moved = true;
     }
 }
