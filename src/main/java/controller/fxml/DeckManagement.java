18e65e67e9d41a47aa9b77f74a019ce47ff98de9
FIRST_NAME LAST_NAME <massimilian.battelli@studio.unibo.it>
2023-04-06 11:58:37 +0200

    Add Listview example

--- src/main/java/controller/fxml/DeckManagement.java
+++ src/main/java/controller/fxml/DeckManagement.java
@@ -1,13 +1,17 @@
 package controller.fxml;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
-public class DeckManagement {
+public class DeckManagement implements Initializable {
     private final SceneHandler sceneHandler = SceneHandler.getInstance();
 
     @FXML
@@ -19,5 +23,15 @@ public class DeckManagement {
             sceneHandler.setPrimaryStage((Stage) backButton.getScene().getWindow());
         }
         sceneHandler.openMainMenu(event);
+    }
+
+    @FXML
+    private ListView<String> rightlist;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        rightlist.getItems().add("Carta 1");
+        rightlist.getItems().add("Carta 2");
+        rightlist.getItems().add("Carta 3");
     }
 }
