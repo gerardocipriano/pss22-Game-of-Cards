4ba84665d6f2e2ffcba51eafdab536d8dd80e9ee
FIRST_NAME LAST_NAME <massimilian.battelli@studio.unibo.it>
2023-04-07 16:17:59 +0200

    Refactor deckmanage classes

--- src/main/java/model/deckmanagement/DeckCell.java
+++ src/main/java/model/deckmanagement/DeckCell.java
@@ -4,30 +4,38 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import model.Deck;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class DeckCell extends ListCell<Deck> {
-
-    private VBox container;
-    private Button deckButton;
     private Label label;
+    private HBox buttonContainer;
+    private VBox container;
+    private Button showButton;
+    private Button deleteButton;
     private RadioButton radioButton;
 
-    public DeckCell(String buttonText, ListView<Deck> leftList, ToggleGroup group  ) {
+    public DeckCell(ListView<Deck> leftList, ToggleGroup group  ) {
         label = new Label();
-        deckButton = new Button(buttonText);
-        radioButton = new RadioButton();
-        radioButton.setText("Scegli");
+        showButton = new Button("Show");
+        deleteButton = new Button("Delete");
+        radioButton = new RadioButton("Select");
         radioButton.setToggleGroup(group);
-        container = new VBox(label, deckButton, radioButton);
-        deckButton.setOnAction(e -> {
+        buttonContainer = new HBox(showButton, deleteButton);
+        container = new VBox(label, buttonContainer, radioButton);
+
+        showButton.setOnAction(e ->{
+            
+        });
+        deleteButton.setOnAction(e -> {
             Deck deck = getItem();
             this.handleDecks(deck, leftList);
         });
     }
+
     protected void handleDecks(Deck deck, ListView<Deck> leftList){
        leftList.getItems().remove(deck);
     }   
