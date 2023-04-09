cdd7ba50fc15a6b2988a600b44607b7ac94bd890
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-04-09 20:44:30 +0200

    [REFACTORING] fix Rules.java

--- src/main/java/controller/fxml/Rules.java
+++ src/main/java/controller/fxml/Rules.java
@@ -22,53 +22,65 @@ import javafx.util.Duration;
 /**
 * Controller for the Rules scene.
 */
-public class Rules  {
-    
-    @FXML
-    private Button backButton;
-    
-    @FXML
-    private TabPane tabPane;
-    
-    @FXML
-    private Text cardCreationText;
-    
-    @FXML
-    private Text deckManagementText;
-    
+/**
+ * This class is the controller for the Rules scene.
+ * It manages the logic for displaying the rules of the game.
+ *
+ * @author @gerardocipriano
+ */
+public class Rules {
+    @FXML private Button backButton;
+    @FXML private TabPane tabPane;
+    @FXML private Text cardCreationText;
+    @FXML private Text deckManagementText;
+
     /**
-    * 
-    * Initializes the controller.
-    * 
-    */
+     * Initializes the controller.
+     * Loads the properties from the file and sets up the TabPane and backButton.
+     */
     public void initialize() {
-        //Get the text rules from the properties external file
+        Properties properties = loadProperties();
+        cardCreationText.setText(properties.getProperty("cardCreation"));
+        deckManagementText.setText(properties.getProperty("deckManagement"));
+        setupTabPane();
+        backButton.setOnAction(event -> {
+            List<ButtonCommand> backCommands = new ArrayList<>();
+            backCommands.add(new ChangeSceneCommand("MainMenu.fxml"));
+            backCommands.add(new PlayClipCommand());
+            MacroCommand decksMacro = new MacroCommand(backCommands);
+            decksMacro.execute();
+        });
+    }
+
+    /**
+     * Loads the properties from the file.
+     *
+     * @return The loaded properties.
+     */
+    private Properties loadProperties() {
         Properties properties = new Properties();
         try (InputStream inputStream = getClass().getResourceAsStream("/rules/rules.properties")) {
             properties.load(inputStream);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        //Set content of labels according to the text in the rules.properties file
-        cardCreationText.setText(properties.getProperty("cardCreation"));
-        deckManagementText.setText(properties.getProperty("deckManagement"));
-        
-        // Set the opacity of the content of all tabs except the first one to 0 at startup
+        return properties;
+    }
+
+    /**
+     * Sets up the TabPane by setting the opacity of the tabs and adding a listener to the selectedItem property.
+     */
+    private void setupTabPane() {
         for (int i = 1; i < tabPane.getTabs().size(); i++) {
             Tab tab = tabPane.getTabs().get(i);
             tab.getContent().setOpacity(0);
         }
-        
-        // Add a listener to the selected item property of the tab pane
         tabPane.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             if (oldValue != null) {
-                // Create and play a fade out transition for the content of the previous tab
                 FadeTransition fadeOut = new FadeTransition(Duration.millis(500), oldValue.getContent());
                 fadeOut.setFromValue(1.0);
                 fadeOut.setToValue(0.0);
                 fadeOut.setOnFinished(event -> {
-                    // Create and play a fade in transition for the content of the new tab
                     FadeTransition fadeIn = new FadeTransition(Duration.millis(500), newValue.getContent());
                     fadeIn.setFromValue(0.0);
                     fadeIn.setToValue(1.0);
@@ -76,20 +88,11 @@ public class Rules  {
                 });
                 fadeOut.play();
             } else {
-                // Create and play a fade in transition for the content of the first tab
                 FadeTransition fadeIn = new FadeTransition(Duration.millis(500), newValue.getContent());
                 fadeIn.setFromValue(0.0);
                 fadeIn.setToValue(1.0);
                 fadeIn.play();
             }
-        });
-
-        backButton.setOnAction(event -> {
-            List<ButtonCommand> backCommands = new ArrayList<>();
-            backCommands.add(new ChangeSceneCommand("MainMenu.fxml"));
-            backCommands.add(new PlayClipCommand());
-            MacroCommand decksMacro = new MacroCommand(backCommands);
-            decksMacro.execute();
         });
     }
 }
