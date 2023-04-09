e537825cbdb63f26bcc30cfe79343229fdaa19eb
FIRST_NAME LAST_NAME <massimilian.battelli@studio.unibo.it>
2023-04-09 22:43:23 +0200

    [DECK] Refactor with command

--- src/main/java/controller/fxml/DeckManagement.java
+++ src/main/java/controller/fxml/DeckManagement.java
@@ -2,53 +2,70 @@ package controller.fxml;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import controller.command.ButtonCommand;
+import controller.command.MacroCommand;
+import controller.command.scene.ChangeSceneCommand;
+import controller.command.sound.PlayClipCommand;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ToggleGroup;
-import javafx.stage.Stage;
 import model.Deck;
 import model.deckmanagement.RightCell;
 import model.deckmanagement.DeckCard;
 import model.deckmanagement.DeckCell;
 import model.deckmanagement.CenterCell;
 
-public class DeckManagement implements Initializable {
-    private final SceneHandler sceneHandler = SceneHandler.getInstance();
+public class DeckManagement {
     
-    @FXML
-    private Button backButton;
+    @FXML private Button backButton;
+    @FXML private ListView<DeckCard> rightList;
+    @FXML private ListView<DeckCard> centerList;
+    @FXML private ListView<Deck> leftList;
 
-    @FXML
-    private ListView<DeckCard> rightList;
+    public void initialize() {
+        List<DeckCard> cards = new ArrayList<>();
+        String json = null;
+        Gson gson = new Gson();
+        ToggleGroup group = new ToggleGroup();
 
-    @FXML
-    private ListView<DeckCard> centerList;
+        //Render the data contained within each cell
+        rightList.setCellFactory(param -> new RightCell("Add", centerList));
+        centerList.setCellFactory(param -> new CenterCell("Remove", centerList));
+        leftList.setCellFactory(param -> new DeckCell(leftList, group));
 
-    @FXML
-    private ListView<Deck> leftList;
-
-    @FXML
-        void backToMainMenu(final ActionEvent event) throws IOException {
-            if (sceneHandler.getPrimaryStage() == null) {
-                sceneHandler.setPrimaryStage((Stage) backButton.getScene().getWindow());
-            }
-            sceneHandler.openMainMenu(event);
+        //Read json file as string
+        try (FileReader reader = new FileReader("./src/main/resources/cards/cards.json")) {
+            json = new BufferedReader(reader).lines().collect(Collectors.joining());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    
+
+        //Convert json to card list and add them to the right listView
+        cards = gson.fromJson(json, new TypeToken<List<DeckCard>>() {}.getType());
+        for (DeckCard card : cards) {
+            rightList.getItems().addAll(card);
+        }
+
+        backButton.setOnAction(event -> {
+            List<ButtonCommand> backCommands = new ArrayList<>();
+            backCommands.add(new ChangeSceneCommand("MainMenu.fxml"));
+            backCommands.add(new PlayClipCommand());
+            MacroCommand decksMacro = new MacroCommand(backCommands);
+            decksMacro.execute();
+        });
+
+    }  
+
     @FXML
         void saveDeck(final ActionEvent event) throws IOException{
             Deck deck = new Deck();
@@ -57,42 +74,17 @@ public class DeckManagement implements Initializable {
             }
             leftList.getItems().add(deck);
         }
-        
-    @Override
-        public void initialize(URL location, ResourceBundle resources) {
-            ToggleGroup group = new ToggleGroup();
-            Gson gson = new Gson();
-            List<DeckCard> cards = new ArrayList<>();
-            String json = null;
-            
-            rightList.setCellFactory(param -> new RightCell("Add", centerList));
-            centerList.setCellFactory(param -> new CenterCell("Remove", centerList));
-            leftList.setCellFactory(param -> new DeckCell(leftList, group));
-
-            // Leggo il json file come stringa
-            try (FileReader reader = new FileReader("./src/main/resources/cards/cards.json")) {
-                json = new BufferedReader(reader).lines().collect(Collectors.joining());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            // Converto il json in una lista di carte
-            cards = gson.fromJson(json, new TypeToken<List<DeckCard>>() {}.getType());
-            //Aggiungo alla lista di destra le carte
-            for (DeckCard card : cards) {
-                rightList.getItems().addAll(card);
-            }
-            /* 
-            Codice per salvare le carte nel json
-            Gson gson = new Gson();
-            String json = gson.toJson(card1);
-            System.out.println(json);
-
-            try (FileWriter writer = new FileWriter("./src/main/resources/cards/cards.json")) {
-                writer.write(json);
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            */
-        }   
 }
+        /* 
+        Codice per salvare le carte nel json
+        Gson gson = new Gson();
+        String json = gson.toJson(card1);
+        System.out.println(json);
+
+        try (FileWriter writer = new FileWriter("./src/main/resources/cards/cards.json")) {
+            writer.write(json);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
