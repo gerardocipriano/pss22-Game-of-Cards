790b08717419689a671af9d5cb7951a3e6509171
Edoardo Triossi <edoardo.triossi@studio.unibo.it>
2023-04-10 16:14:01 +0200

    made sure that match can be used by controll

--- src/main/java/controller/fxml/Match.java
+++ src/main/java/controller/fxml/Match.java
@@ -2,14 +2,21 @@ package controller.fxml;
 
 import java.io.IOException;
 
+import controller.fxml.SceneHandler;
 import controller.sound.BackgroundMusic;
 import controller.sound.SoundButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
 import model.Deck;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import model.match.*;
 
-public class Match {
+public class Match implements Initializable {
+
+    IMatchModel matchModel = MatchModel.getInstance();
 
     private final SceneHandler sceneHandler = SceneHandler.getInstance();
 
@@ -25,15 +32,10 @@ public class Match {
         BackgroundMusic.getInstance().playMainTheme();
     }
 
-    void startMatch(Deck deck) {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        BackgroundMusic.getInstance().playMainTheme();
 
-        createGameBoard();
-
-        getDeck();
-
-        createPlayer1();
-        createPlayer2();
-
+        // matchModel->start();
     }
-
 }
--- src/main/java/launcher/GameStart.java
+++ src/main/java/launcher/GameStart.java
@@ -7,12 +7,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+// import model.match.Match;
 import controller.sound.*;
+import controller.fxml.*;
+import model.Deck;
 
 /**
  * This class sets up the GUI for the Main Menu of the application.
+ * 
+ * Controller Class
  */
 public final class GameStart extends Application {
+
+    // GameViewInterface view;
+
     /**
      * 
      */
@@ -20,15 +28,21 @@ public final class GameStart extends Application {
     private static final int SCENE_WIDTH = 1280;
     private static final int SCENE_HEIGHT = 720;
     private final Stage primaryStage = new Stage();
+
+    // private Match match = new Match();
+
     /**
      * This method loads the Menu and sets the window up.
      * 
      * @param stage the loaded stage.
      */
     public void start(final Stage stage) throws IOException {
+
+        // gameView new GameView(this);
+
         final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/MainMenuGui.fxml"));
         final Scene scene = new Scene(root);
-        
+
         primaryStage.getIcons().add(new Image("/images/logo.png"));
         primaryStage.setScene(scene);
         primaryStage.setTitle("Game of Cards");
@@ -36,11 +50,24 @@ public final class GameStart extends Application {
         primaryStage.setWidth(SCENE_WIDTH);
         primaryStage.setHeight(SCENE_HEIGHT);
         primaryStage.setAlwaysOnTop(true);
+
+        SceneHandler.getInstance().add(this);
+
+        // match = new Match();
+
+        // gameView.Show();
         primaryStage.show();
         primaryStage.setResizable(false);
         BackgroundMusic.getInstance().playMainTheme();
         BackgroundMusic.getInstance().setVolume(0.025);
     }
+
+    // public void startMatch() throws IOException {
+    // Deck deck = new Deck();
+
+    // match.start(deck);
+    // }
+
     /**
      * Main method.
      * 
