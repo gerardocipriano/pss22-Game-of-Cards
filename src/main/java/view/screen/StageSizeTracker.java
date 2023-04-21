17bef7c286de45da89131496b349bef973031bc2
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-04-21 13:34:20 +0200

    [LAUNCHER] Added StageSizeTracker

---
+++ src/main/java/view/screen/StageSizeTracker.java
@@ -0,0 +1,40 @@
+package view.screen;
+
+
+import javafx.stage.Stage;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
+public class StageSizeTracker {
+    private static double stageWidth;
+    private static double stageHeight;
+
+    public StageSizeTracker(Stage stage) {
+        stageWidth = stage.getWidth();
+        stageHeight = stage.getHeight();
+
+        stage.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
+                stageWidth = (double) newSceneWidth;
+                System.out.println(stageWidth);
+            }
+        });
+
+        stage.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
+                stageHeight = (double) newSceneHeight;
+                System.out.println(stageHeight);
+            }
+        });
+    }
+
+    public static double getStageWidth() {
+        return stageWidth;
+    }
+
+    public static double getStageHeight() {
+        return stageHeight;
+    }
+}
