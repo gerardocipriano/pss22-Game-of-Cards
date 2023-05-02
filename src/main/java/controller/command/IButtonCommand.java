5f9c5ba455a6c07d99b7b90a9aea1f1882ea789d
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-05-02 21:35:56 +0200

    [REFACTORING] Removed unused imports

--- src/main/java/controller/command/IButtonCommand.java
+++ src/main/java/controller/command/IButtonCommand.java
@@ -1,17 +1,16 @@
 package controller.command;
 
-import model.Card;
-
 /**
-* This interface represents a generic button command.
-* The implementation of this interface should define the logic of the command that will be
-* executed when the button is pressed.
-*
-* @author gerardocipriano
-*/
+ * This interface represents a generic button command.
+ * The implementation of this interface should define the logic of the command
+ * that will be
+ * executed when the button is pressed.
+ *
+ * @author gerardocipriano
+ */
 public interface IButtonCommand {
     /**
-    * Executes the command implemented by the implementation class.
-    */
+     * Executes the command implemented by the implementation class.
+     */
     void execute();
 }
--- src/main/java/controller/fxml/Rules.java
+++ src/main/java/controller/fxml/Rules.java
@@ -1,19 +1,11 @@
 package controller.fxml;
 
-import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.util.Duration;
 import view.rules.TabPaneSetupView;
-import view.rules.TabViewFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 import controller.command.IButtonCommand;
 import controller.command.MacroCommand;
@@ -46,21 +38,6 @@ public class Rules {
             MacroCommand decksMacro = new MacroCommand(backCommands);
             decksMacro.execute();
         });
-    }
-
-    /**
-     * Loads the properties from the properties file.
-     *
-     * @return the loaded properties
-     */
-    public Properties loadProperties() {
-        Properties properties = new Properties();
-        try (InputStream inputStream = getClass().getResourceAsStream("/rules/rules.properties")) {
-            properties.load(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return properties;
     }
 
     /**
