1bdf82866be79331c601a95ffb6146f93e676004
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-06-28 11:48:31 +0200

    added writedecks persistency

--- src/main/java/utilities/parser/DeckParser.java
+++ src/main/java/utilities/parser/DeckParser.java
@@ -1,24 +1,27 @@
 package utilities.parser;
 
- 
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import model.Deck;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DeckParser {
+    private static final String DECKS_FILE = "decks.json";
+
     public static void writeDecks(List<Deck> decks) {
         Gson gson = new Gson();
         String json = gson.toJson(decks);
         System.out.println(json);
-        try (OutputStream os = new FileOutputStream("./src/main/resources/json/decks.json");
-             OutputStreamWriter writer = new OutputStreamWriter(os)) {
+        Path path = Paths.get(DECKS_FILE);
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
             writer.write(json);
         } catch (IOException e) {
             e.printStackTrace();
@@ -29,111 +32,33 @@ public class DeckParser {
         List<Deck> decks = new ArrayList<>();
         String json = null;
         Gson gson = new Gson();
-        try (InputStream is = DeckParser.class.getResourceAsStream("/json/decks.json");
-             InputStreamReader reader = new InputStreamReader(is)) {
-            json = new BufferedReader(reader).lines().collect(Collectors.joining());
+        Path path = Paths.get(DECKS_FILE);
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            json = reader.lines().collect(Collectors.joining());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        decks = gson.fromJson(json, new TypeToken<List<Deck>>() {});
+        decks = gson.fromJson(json, new TypeToken<List<Deck>>() {
+        });
         return decks;
     }
 
- 
-
     public static void deleteDeck(Deck deck) {
         String json = null;
-        try (InputStream is = DeckParser.class.getResourceAsStream("/json/decks.json");
-             InputStreamReader reader = new InputStreamReader(is)) {
-            json = new BufferedReader(reader).lines().collect(Collectors.joining());
+        Path path = Paths.get(DECKS_FILE);
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            json = reader.lines().collect(Collectors.joining());
         } catch (IOException e) {
             e.printStackTrace();
             return;
         }
-        List<Deck> decks = new Gson().fromJson(json, new TypeToken<List<Deck>>() {});
+        List<Deck> decks = new Gson().fromJson(json, new TypeToken<List<Deck>>() {
+        });
         decks.removeIf(d -> d.getName().equals(deck.getName()));
-        try (FileWriter writer = new FileWriter("./src/main/resources/json/decks.json")) {
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
             new Gson().toJson(decks, writer);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
-
-/*
-package utilities.parser;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import model.Deck;
-/**
- * Class that has the task of reading and writing the decks in its own json file
- *
- * @author massimilianobattelli
- */
-/*
-public class DeckParser {
-
-    public static void writeDecks(List<Deck> decks){
-        Gson gson = new Gson();
-        String json = gson.toJson(decks);
-        System.out.println(json);
-    
-        try (FileWriter writer = new FileWriter("./src/main/resources/json/decks.json")) {
-            writer.write(json);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public static List<Deck> parseDecks(){
-        List<Deck> decks = new ArrayList<>();
-        String json = null;
-        Gson gson = new Gson();
-
-        try (FileReader reader = new FileReader("./src/main/resources/json/decks.json")) {
-            json = new BufferedReader(reader).lines().collect(Collectors.joining());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        decks = gson.fromJson(json, new TypeToken<List<Deck>>() {});
-        return decks;
-    }
-
-    public static void deleteDeck(Deck deck) {
-        // Read the existing JSON data from the file into a string
-        String json = null;
-
-        try (FileReader reader = new FileReader("./src/main/resources/json/decks.json")) {
-            json = new BufferedReader(reader).lines().collect(Collectors.joining());
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-    
-        // Parse the JSON data into a list of Deck objects
-        List<Deck> decks = new Gson().fromJson(json, new TypeToken<List<Deck>>() {});
-    
-        // Find the deck to delete and remove it from the list
-        decks.removeIf(d -> d.getName().equals(deck.getName()));
-    
-        // Write the updated list of decks back to the file
-        try (FileWriter writer = new FileWriter("./src/main/resources/json/decks.json")) {
-            new Gson().toJson(decks, writer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-}
- */
